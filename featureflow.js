6cfaf86b8b59e310f09019a8698d9e92c68e2d77
Henry Young <henry.young@industrieit.com>
2017-03-28 13:26:38 +1100

    v0.3.0

--- featureflow.js
+++ featureflow.js
@@ -1,7 +1,7 @@
 /*!
- * Featureflow Client v0.2.5
+ * Featureflow Client v0.3.0
  * Web: https://www.featureflow.io/
- * Date: 2017-03-23T22:53:51.004Z
+ * Date: 2017-03-28T02:26:33.527Z
  * Licence: Apache-2.0
  */
 (function webpackUniversalModuleDefinition(root, factory) {
@@ -79,7 +79,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	__webpack_require__.p = "";
 
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 7);
+/******/ 	return __webpack_require__(__webpack_require__.s = 8);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -108,8 +108,10 @@ var UPDATED_FEATURE = 'UPDATED_FEATURE';
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__RestClient__ = __webpack_require__(4);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__Evaluate__ = __webpack_require__(3);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__Events__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_tiny_emitter__ = __webpack_require__(5);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_tiny_emitter__ = __webpack_require__(6);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_tiny_emitter___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3_tiny_emitter__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_js_cookie__ = __webpack_require__(5);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_js_cookie___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_4_js_cookie__);
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -123,9 +125,6 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 
 
-var DEFAULT_CONTEXT_VALUES = {
-  key: 'anonymous'
-};
 
 var DEFAULT_BASE_URL = 'https://app.featureflow.io';
 var DEFAULT_RTM_URL = 'https://rtm.featureflow.io';
@@ -134,7 +133,8 @@ var DEFAULT_CONFIG = {
   baseUrl: DEFAULT_BASE_URL,
   rtmUrl: DEFAULT_RTM_URL,
   streaming: true,
-  defaultFeatures: {}
+  defaultFeatures: {},
+  useCookies: true
 };
 
 var INIT_MODULE_ERROR = new Error('init() has not been called with a valid apiKey');
@@ -215,7 +215,7 @@ var FeatureflowClient = function () {
       var callback = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : function () {};
 
       this.context = {
-        key: context.key || DEFAULT_CONTEXT_VALUES.key,
+        key: context.key || this.getAnonymousKey(),
         values: context.values
       };
 
@@ -252,7 +252,24 @@ var FeatureflowClient = function () {
   }, {
     key: 'goal',
     value: function goal(_goal) {
-      return __WEBPACK_IMPORTED_MODULE_0__RestClient__["a" /* default */].postGoalEvent(this.config.baseUrl, this.apiKey, _goal, this.getFeatures(), function () {});
+      return __WEBPACK_IMPORTED_MODULE_0__RestClient__["a" /* default */].postGoalEvent(this.config.baseUrl, this.apiKey, this.context.key, _goal, this.getFeatures(), function () {});
+    }
+  }, {
+    key: 'getAnonymousKey',
+    value: function getAnonymousKey() {
+      return localStorage.getItem('ff-anonymous-key') || this.resetAnonymousKey();
+    }
+  }, {
+    key: 'resetAnonymousKey',
+    value: function resetAnonymousKey() {
+      var anonymousKey = 'anonymous:' + Math.random().toString(36).substring(10);
+      localStorage.setItem('ff-anonymous-key', anonymousKey);
+
+      if (this.config.useCookies) {
+        //Set the anonymous key cookie for potential future usage with Server SDK
+        __WEBPACK_IMPORTED_MODULE_4_js_cookie___default.a.set('ff-anonymous-key', anonymousKey);
+      }
+      return anonymousKey;
     }
   }]);
 
@@ -339,7 +356,7 @@ var Evaluate = function () {
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__package_json__ = __webpack_require__(6);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__package_json__ = __webpack_require__(7);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__package_json___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__package_json__);
 
 
@@ -378,11 +395,12 @@ function base64URLEncode(context) {
     var query = keys.length > 0 ? '?keys=' + keys.join(',') : '';
     request(baseUrl + '/api/js/v1/evaluate/' + apiKey + '/context/' + encodeURI(base64URLEncode(context)) + query, { method: 'GET' }, callback);
   },
-  postGoalEvent: function postGoalEvent(baseUrl, apiKey, key, evaluated, callback) {
+  postGoalEvent: function postGoalEvent(baseUrl, apiKey, contextKey, goalKey, evaluated, callback) {
     request(baseUrl + '/api/js/v1/goalevent/' + apiKey, {
       method: 'POST',
       body: {
-        key: key,
+        contextKey: contextKey,
+        goalKey: goalKey,
         hits: 1,
         evaluated: evaluated
       }
@@ -392,6 +410,172 @@ function base64URLEncode(context) {
 
 /***/ }),
 /* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_RESULT__;/*!
+ * JavaScript Cookie v2.1.3
+ * https://github.com/js-cookie/js-cookie
+ *
+ * Copyright 2006, 2015 Klaus Hartl & Fagner Brack
+ * Released under the MIT license
+ */
+;(function (factory) {
+	var registeredInModuleLoader = false;
+	if (true) {
+		!(__WEBPACK_AMD_DEFINE_FACTORY__ = (factory),
+				__WEBPACK_AMD_DEFINE_RESULT__ = (typeof __WEBPACK_AMD_DEFINE_FACTORY__ === 'function' ?
+				(__WEBPACK_AMD_DEFINE_FACTORY__.call(exports, __webpack_require__, exports, module)) :
+				__WEBPACK_AMD_DEFINE_FACTORY__),
+				__WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
+		registeredInModuleLoader = true;
+	}
+	if (true) {
+		module.exports = factory();
+		registeredInModuleLoader = true;
+	}
+	if (!registeredInModuleLoader) {
+		var OldCookies = window.Cookies;
+		var api = window.Cookies = factory();
+		api.noConflict = function () {
+			window.Cookies = OldCookies;
+			return api;
+		};
+	}
+}(function () {
+	function extend () {
+		var i = 0;
+		var result = {};
+		for (; i < arguments.length; i++) {
+			var attributes = arguments[ i ];
+			for (var key in attributes) {
+				result[key] = attributes[key];
+			}
+		}
+		return result;
+	}
+
+	function init (converter) {
+		function api (key, value, attributes) {
+			var result;
+			if (typeof document === 'undefined') {
+				return;
+			}
+
+			// Write
+
+			if (arguments.length > 1) {
+				attributes = extend({
+					path: '/'
+				}, api.defaults, attributes);
+
+				if (typeof attributes.expires === 'number') {
+					var expires = new Date();
+					expires.setMilliseconds(expires.getMilliseconds() + attributes.expires * 864e+5);
+					attributes.expires = expires;
+				}
+
+				try {
+					result = JSON.stringify(value);
+					if (/^[\{\[]/.test(result)) {
+						value = result;
+					}
+				} catch (e) {}
+
+				if (!converter.write) {
+					value = encodeURIComponent(String(value))
+						.replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g, decodeURIComponent);
+				} else {
+					value = converter.write(value, key);
+				}
+
+				key = encodeURIComponent(String(key));
+				key = key.replace(/%(23|24|26|2B|5E|60|7C)/g, decodeURIComponent);
+				key = key.replace(/[\(\)]/g, escape);
+
+				return (document.cookie = [
+					key, '=', value,
+					attributes.expires ? '; expires=' + attributes.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
+					attributes.path ? '; path=' + attributes.path : '',
+					attributes.domain ? '; domain=' + attributes.domain : '',
+					attributes.secure ? '; secure' : ''
+				].join(''));
+			}
+
+			// Read
+
+			if (!key) {
+				result = {};
+			}
+
+			// To prevent the for loop in the first place assign an empty array
+			// in case there are no cookies at all. Also prevents odd result when
+			// calling "get()"
+			var cookies = document.cookie ? document.cookie.split('; ') : [];
+			var rdecode = /(%[0-9A-Z]{2})+/g;
+			var i = 0;
+
+			for (; i < cookies.length; i++) {
+				var parts = cookies[i].split('=');
+				var cookie = parts.slice(1).join('=');
+
+				if (cookie.charAt(0) === '"') {
+					cookie = cookie.slice(1, -1);
+				}
+
+				try {
+					var name = parts[0].replace(rdecode, decodeURIComponent);
+					cookie = converter.read ?
+						converter.read(cookie, name) : converter(cookie, name) ||
+						cookie.replace(rdecode, decodeURIComponent);
+
+					if (this.json) {
+						try {
+							cookie = JSON.parse(cookie);
+						} catch (e) {}
+					}
+
+					if (key === name) {
+						result = cookie;
+						break;
+					}
+
+					if (!key) {
+						result[name] = cookie;
+					}
+				} catch (e) {}
+			}
+
+			return result;
+		}
+
+		api.set = api;
+		api.get = function (key) {
+			return api.call(api, key);
+		};
+		api.getJSON = function () {
+			return api.apply({
+				json: true
+			}, [].slice.call(arguments));
+		};
+		api.defaults = {};
+
+		api.remove = function (key, attributes) {
+			api(key, '', extend(attributes, {
+				expires: -1
+			}));
+		};
+
+		api.withConverter = init;
+
+		return api;
+	}
+
+	return init(function () {});
+}));
+
+
+/***/ }),
+/* 6 */
 /***/ (function(module, exports) {
 
 function E () {
@@ -463,12 +647,12 @@ module.exports = E;
 
 
 /***/ }),
-/* 6 */
+/* 7 */
 /***/ (function(module, exports) {
 
 module.exports = {
 	"name": "featureflow-client",
-	"version": "0.2.5",
+	"version": "0.3.0",
 	"description": "Featureflow Javascipt Client",
 	"author": "Featureflow <featureflow@featureflow.io>",
 	"license": "Apache-2.0",
@@ -493,7 +677,8 @@ module.exports = {
 		"watch": "webpack --watch src/index.js dist/featureflow.js",
 		"test": "karma start --single-run",
 		"test:watch": "karma start --auto-watch --reporters=dots",
-		"prepublish": "node ./scripts/prepublish.js",
+		"prepublish": "node ./scripts/build.js",
+		"publish:bower": "node ./scripts/bower.js",
 		"flow": "flow",
 		"example": "webpack-dev-server --config ./example/webpack.config.js --content-base example/ --inline --port 8182"
 	},
@@ -532,12 +717,13 @@ module.exports = {
 		"webpack-dev-server": "^2.3.0"
 	},
 	"dependencies": {
+		"js-cookie": "^2.1.3",
 		"tiny-emitter": "^1.1.0"
 	}
 };
 
 /***/ }),
-/* 7 */
+/* 8 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
